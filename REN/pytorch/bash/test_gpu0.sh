573d5b5cf90783278165b29dca55e7fa6342e99d
Lin <1003389754ll@gmail.com>
2021-08-21 12:06:32 +0800

    Delete REN directory

--- REN/pytorch/bash/test_gpu0.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-python test_image.py --net ResNet50 --dset office  --s_dset_path ../data/office/webcam_list.txt   \\
-  --t_dset_path ../data/office/amazon_list.txt --model_dir ./logs/con2/webcam2amazon_CDAN --gpu_id 0
--- REN/pytorch/bash/visda.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-python train_image.py --net ResNet50 --dset visda --test_interval 500 --s_dset_path ../data/visda-2017/train_list.txt \
-  --t_dset_path ../data/visda-2017/validation_list.txt CDAN --output_path visda1 --gpu_id 1
-python train_image.py --net ResNet50 --dset visda --test_interval 500 --s_dset_path ../data/visda-2017/train_list.txt \
-  --t_dset_path ../data/visda-2017/validation_list.txt CDAN --output_path visda2 --gpu_id 1
-python train_image.py --net ResNet50 --dset visda --test_interval 500 --s_dset_path ../data/visda-2017/train_list.txt \
-  --t_dset_path ../data/visda-2017/validation_list.txt CDAN --output_path visda3 --gpu_id 1
